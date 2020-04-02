6cdb68d13daaea5c2ef2cbfc04761ec364fa2f8c
fynnliu <fynnliu@tencent.com>
2020-04-02 21:07:29 +0800

    双指针遍历法

---
+++ array/move_zero_test.go
@@ -0,0 +1,25 @@
+package array
+
+import "testing"
+
+func Test_moveZeroes(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "case1",
+			args: args{
+				nums: []int{0, 1, 0, 3, 12},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			moveZeroes(tt.args.nums)
+		})
+	}
+}
