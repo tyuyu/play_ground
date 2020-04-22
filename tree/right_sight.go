8031fce4a91f100abe96487a2d73f34ac0116de8
fynnliu <fynnliu@tencent.com>
2020-04-22 09:41:15 +0800

    深度优先遍历，100%defeat

---
+++ tree/right_sight.go
@@ -0,0 +1,61 @@
+package tree
+
+//给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+//
+//示例:
+//
+//输入: [1,2,3,null,5,null,4]
+//输出: [1, 3, 4]
+//解释:
+//
+//   1            <---
+// /   \
+//2     3         <---
+// \     \
+//  5     4       <---
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/binary-tree-right-side-view
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+func rightSideView(root *TreeNode) []int {
+
+	if root == nil {
+		return []int{}
+	}
+
+	level := make([][]int, 0)
+	deph(root, &level, 0)
+	res := make([]int, 0)
+	for _, l := range level {
+		res = append(res, l[0])
+	}
+	return res
+}
+
+func deph(root *TreeNode, levels *[][]int, deep int) {
+	if len(*levels) < deep+1 {
+		*levels = append(*levels, []int{})
+	}
+	if root.Right != nil {
+		deph(root.Right, levels, deep+1)
+	}
+	if root.Left != nil {
+		deph(root.Left, levels, deep+1)
+	}
+	(*levels)[deep] = append((*levels)[deep], root.Val)
+}
+
+//执行结果：
+//通过
+//显示详情
+//执行用时 :
+//0 ms
+//, 在所有 Go 提交中击败了
+//100.00%
+//的用户
+//内存消耗 :
+//2.3 MB
+//, 在所有 Go 提交中击败了
+//100.00%
+//的用户
---
+++ tree/right_sight_test.go
@@ -0,0 +1,52 @@
+package tree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_rightSideView(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "case1",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val:  2,
+						Left: nil,
+						Right: &TreeNode{
+							Val:   5,
+							Left:  nil,
+							Right: nil,
+						},
+					},
+					Right: &TreeNode{
+						Val:  3,
+						Left: nil,
+						Right: &TreeNode{
+							Val:   4,
+							Left:  nil,
+							Right: nil,
+						},
+					},
+				},
+			},
+			want: []int{1, 3, 4},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rightSideView(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("rightSideView() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
