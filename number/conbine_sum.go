81fcd96e6e7368dda1d5b44e39f8d712a18e899b
fynnliu <fynnliu@tencent.com>
2020-04-30 16:46:25 +0800

    深度遍历

--- number/conbine_sum.go
+++ number/conbine_sum.go
@@ -56,3 +56,61 @@ func dfs(candidates []int, last []int, target, index int, res *[][]int) {
 		dfs(candidates, next, target-num, i, res)
 	}
 }
+
+//给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+//
+//candidates 中的每个数字在每个组合中只能使用一次。
+//
+//说明：
+//
+//所有数字（包括目标数）都是正整数。
+//解集不能包含重复的组合。
+//示例 1:
+//
+//输入: candidates = [10,1,2,7,6,1,5], target = 8,
+//所求解集为:
+//[
+//  [1, 7],
+//  [1, 2, 5],
+//  [2, 6],
+//  [1, 1, 6]
+//]
+//示例 2:
+//
+//输入: candidates = [2,5,2,1,2], target = 5,
+//所求解集为:
+//[
+//  [1,2,2],
+//  [5]
+//]
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/combination-sum-ii
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+func combinationSum2(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+	res := new([][]int)
+	dsf2(candidates, []int{}, target, res)
+	return *res
+}
+
+func dsf2(candidates []int, old []int, target int, res *[][]int) {
+
+	if target == 0 {
+		*res = append(*res, old)
+		return
+	}
+
+	for i, num := range candidates {
+		if num > target {
+			return
+		}
+		if i > 0 && num == candidates[i-1] {
+			continue
+		}
+		nc := candidates[i+1:]
+		next := append(old, num)
+		dsf2(nc, next, target-num, res)
+	}
+}
--- number/conbine_sum_test.go
+++ number/conbine_sum_test.go
@@ -32,3 +32,38 @@ func Test_combinationSum(t *testing.T) {
 		})
 	}
 }
+
+func Test_combinationSum2(t *testing.T) {
+	type args struct {
+		candidates []int
+		target     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "case1",
+			args: args{
+				candidates: []int{10, 1, 2, 7, 6, 1, 5},
+				target:     8,
+			},
+			want: nil,
+		}, {
+			name: "case2",
+			args: args{
+				candidates: []int{2, 5, 2, 1, 2},
+				target:     5,
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := combinationSum2(tt.args.candidates, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("combinationSum2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
