3d3826e236452936df890258961189fb7b77c93d
fynnliu <fynnliu@tencent.com>
2020-05-15 22:33:09 +0800

    ssss

---
+++ dynamic/sub_sum_test.go
@@ -0,0 +1,31 @@
+package dynamic
+
+import "testing"
+
+func Test_subarraySum(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case1",
+			args: args{
+				nums: []int{100, 1, 2, 3, 4},
+				k:    6,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := subarraySum(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("subarraySum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
